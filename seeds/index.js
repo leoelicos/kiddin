394a833df8a86b5a2f66bcb56ebe81767ce93813
Leo Wong <leoelicos@gmail.com>
2022-05-30 23:00:31 +1000

    Change order of seeding - move seedPosts last

--- seeds/index.js
+++ seeds/index.js
@@ -6,15 +6,12 @@ const sequelize = require('../config/connection');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-  //   console.log('\n----- DATABASE SYNCED -----\n');
-  await seedPosts();
-  //   console.log('\n----- CATEGORIES SEEDED -----\n');
 
   await seedThreads();
-  //   console.log('\n----- PRODUCTS SEEDED -----\n');
 
   await seedUsers();
-  //   console.log('\n----- TAGS SEEDED -----\n');
+
+  await seedPosts();
 
   process.exit(0);
 };
