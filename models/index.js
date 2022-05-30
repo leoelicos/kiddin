3897362f1af4733d8e1b1aedc20851ead8a72201
Key <jh.yield@gmail.com>
2022-05-30 21:19:06 +1000

    making index file for models connection

---
+++ models/index.js
@@ -0,0 +1,25 @@
+const Thread = require('./Thread');
+const User = require('./User');
+const Post = require('./Post');
+
+Post.belongsTo(Thread, {
+  foreignKey: 'thread_id',
+});
+
+Thread.hasMany(Post, {
+  foreignKey: 'thread_id',
+  onDelete: 'CASCADE',
+});
+
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+
+module.exports = { Thread, User, Post };
