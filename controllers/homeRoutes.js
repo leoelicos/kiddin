5d18ef641e2574c9e7c178109786201f89a62e0c
Key <jh.yield@gmail.com>
2022-05-31 21:17:19 +1000

    cleaned up eslint fixes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,44 +3,44 @@ const { Thread } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-    try {
-        const threadData = await Thread.findAll();
+  try {
+    const threadData = await Thread.findAll();
 
-        const threads = threadData.map((thread) => thread.get({ plain: true }));
+    const threads = threadData.map((thread) => thread.get({ plain: true }));
 
-        res.render('homepage', {
-            threads,
-            logged_in: req.session.logged_in
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+    res.render('homepage', {
+      threads,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/thread/:id', withAuth, async (req, res) => {
-    try {
-        const threadData = await Thread.findByPk(req.params.id);
+  try {
+    const threadData = await Thread.findByPk(req.params.id);
 
-        const threads = threadData.get({ plain: true });
+    const threads = threadData.get({ plain: true });
 
-        res.render('threads', {
-            ...threads,
-            logged_in: req.session.logged_in
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+    res.render('threads', {
+      ...threads,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 
 router.get('/login', (req, res) => {
 
-    if (req.session.logged_in) {
-        res.redirect('/');
-        return;
-    }
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
 
-    res.render('login');
+  res.render('login');
 });
 
 module.exports = router;
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,20 +3,20 @@ const { User } = require('../models');
 
 const userData = [
   {
-   username:'Jess',
-   email:'jess@jess.com',
-   password:'jessiscool'
+    username:'Jess',
+    email:'jess@jess.com',
+    password:'jessiscool'
   },
   {
     username:'Leo',
     email:'leo@leo.com',
     password:'leoiscool'
-   },
-   {
+  },
+  {
     username:'Ali',
     email:'ali@ali.com',
     password:'aliiscool'
-   }
+  }
 
 ];
 
